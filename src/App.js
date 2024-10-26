3510b23396c42d76f0f2ae9f3ee5d763d832af5b
Tanjiro <kamado.nql2109@gmail.com>
2024-10-26 21:21:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
     getCountry("https://countriesnow.space/api/v0.1/countries");
   }, []);
 
+  // Fetch country data from API
   const getCountry = async (url) => {
     const responsive = await axios.get(url).then((res) => {
       setCountryList(res.data.data);
